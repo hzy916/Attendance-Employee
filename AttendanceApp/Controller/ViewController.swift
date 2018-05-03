c4ec02e00b1377d61c372d2940bca05d8c55b781
hzy916 <zh29@st-andrews.ac.uk>
2018-05-03 11:03:52 +0100

    create current array to reload the collection view

--- AttendanceApp/Controller/ViewController.swift
+++ AttendanceApp/Controller/ViewController.swift
@@ -15,7 +15,26 @@ class ViewController: UIViewController, UICollectionViewDelegate,UICollectionVie
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     
-    var empArray: [Employee] = []
+    @IBAction func changeView(_ sender: UISegmentedControl) {
+        switch segmentedControl.selectedSegmentIndex
+        {
+        case 0:
+             currentArray = checkinArray
+             collectionView.reloadData()
+        case 1:
+             currentArray = checkoutArray
+             collectionView.reloadData()
+        default:
+            break;
+        }
+    
+    }
+    
+    
+    var checkinArray: [Employee] = []
+    var checkoutArray: [Employee] = []
+    var currentArray: [Employee] = []
+    
     
     override func viewDidLoad() {
         collectionView.delegate = self
@@ -25,9 +44,9 @@ class ViewController: UIViewController, UICollectionViewDelegate,UICollectionVie
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        empArray = loadEmployeeDetails()
+        checkinArray = loadEmployeeDetails()
         
-        print(empArray)
+        print(checkinArray)
         
         collectionView.reloadData()
     }
@@ -42,34 +61,34 @@ class ViewController: UIViewController, UICollectionViewDelegate,UICollectionVie
         
         //Read from plist
         
-        var empArray: [Employee] = []
+        var checkinArray: [Employee] = []
          if let path = Bundle.main.path(forResource: "Employees", ofType: "plist") {
             if let englishFromPlist = NSArray(contentsOfFile: path) as? [Dictionary<String, Any>] {
                 for item in englishFromPlist {
                     let employee = Employee(name: item["name"] as! String, department: item["deparment"] as! String, status: false, inTime: "", outTime: "")
-                    empArray.append(employee)
+                    checkinArray.append(employee)
                 }
             }
         }
 
-       return empArray
+       return checkinArray
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-            return empArray.count
+            return checkinArray.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "customCell", for: indexPath) as! CustomCollectionViewCell
         
-        let employee = empArray[indexPath.row] as Employee
+        let employee = checkinArray[indexPath.row] as Employee
         cell.lblCell.text = employee.employeeName
         
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print("user tapped on image # \(empArray[indexPath.row])")
+        print("user tapped on image # \(checkinArray[indexPath.row])")
         
         let mySignInViewPage: SignInViewController = self.storyboard?.instantiateViewController(withIdentifier: "SignInViewController") as! SignInViewController
         
@@ -77,6 +96,10 @@ class ViewController: UIViewController, UICollectionViewDelegate,UICollectionVie
         
         self.navigationController?.pushViewController(mySignInViewPage, animated: true)
         
+        //remove employee from check in view
+        checkinArray.remove(at: indexPath.row)
+        //add employee to checkout view
+        checkoutArray.append(checkinArray[indexPath.row])
     }
 
     
