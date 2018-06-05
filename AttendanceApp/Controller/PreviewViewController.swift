e18b8a1ebd78116785f5de3d6977f0d453358a91
hzy916 <zh29@st-andrews.ac.uk>
2018-06-05 15:26:16 +0100

    image in the same HTML file then it works. next : change the save signature image file path.

--- AttendanceApp/Controller/PreviewViewController.swift
+++ AttendanceApp/Controller/PreviewViewController.swift
@@ -22,18 +22,15 @@ class PreviewViewController: UIViewController {
     
     var HTMLContent: String!
     
-    //var reportArray: [Employee] = []
-    
 
     @IBOutlet weak var webPreview: UIWebView!
     
     var reportArray = [[String: String]]()
-//    let employeeArray = [employeeName: "hello", reportDate:"2018"]
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+     // Do any additional setup after loading the view.
        reportArray  = readReportdata()
         print(reportArray)
     }
@@ -46,35 +43,11 @@ class PreviewViewController: UIViewController {
     //load employee details from array
     func readReportdata() -> Array<[String: String]> {
         //Read from plist
-      
-        //let reportFromPlist = NSArray(contentsOfFile: path) as? [Dictionary<String, Any>]
-        
-       // if let path = Bundle.main.path(forResource: "time", ofType: "plist") {
-       //     print(dataFilePath)
-        
-//        if !fileManager.fileExists(atPath: path) {
-//            print("File not exist!")
-//
-//            let srcPath = Bundle.main.path(forResource: "time", ofType: "plist")
-//
-//            do {
-//                //Copy the project plist file to the documents directory.
-//                try fileManager.copyItem(atPath: srcPath!, toPath: path)
-//            } catch {
-//                print("File copy error!")
-//            }
-//        }
-       
         
         if let reportFromPlist = NSArray(contentsOfFile: path) as? [[String: String]] {
-//                for item in reportFromPlist {
-//                    //let employeeData = Employee(name: item["name"] as! String, department: item["deparment"] as! String, status: false, inTime: item["checkinTime"] as! String, outTime: item["checkoutTime"]as! String)
-//                    reportArray.append(employeeData)
-//                    print(employeeData)
-//                }
+
                 reportArray = reportFromPlist
             }
-       // }
         return reportArray
     }
     
--- AttendanceApp/Controller/ReportComposer.swift
+++ AttendanceApp/Controller/ReportComposer.swift
@@ -30,42 +30,49 @@ class ReportComposer: NSObject {
     let dueDate = ""
     
     let logoImageURL = "http://www.appcoda.com/wp-content/uploads/2015/12/blog-logo-dark-400.png"
-
-//    let fileURL = Bundle.main.url(forResource: "AttitudeTech", withExtension: "png")
+    
+    
+    let imagepath = NSHomeDirectory()+"/Documents/"
+    var dictionary : NSMutableDictionary!
+    let fileManager = FileManager.default
+//  let fileURL = Bundle.main.url(forResource: "AttitudeTech", withExtension: "png")
 
 
     var pdfFilename: String!
 
-//    //Mark get current date
-//    func GetCurrentDate() -> String {
-//        let dateFormatter = DateFormatter()
-//        dateFormatter.dateStyle = DateFormatter.Style.medium
-//        return dateFormatter.string(from: Date())
-//    }
-//    var reportDate = GetCurrentDate()
-//
-//    let path = NSHomeDirectory()+"/Documents/time.plist"
-//    var dictionary : NSMutableDictionary!
-//    let fileManager = FileManager.default
-//
-    
     func renderReport(items:[[String: String]]) -> String!{
         do {
             // Load the invoice HTML template code into a String variable.
             var HTMLContent = try String(contentsOfFile: pathToInvoiceHTMLTemplate!)
-            
+    
             // Replace all the placeholders with real values except for the items.
             // The logo image.
             HTMLContent = HTMLContent.replacingOccurrences(of: "#LOGO_IMAGE#", with: logoImageURL)
             
-//            // Report date.
-//            HTMLContent = HTMLContent.replacingOccurrences(of: "#INVOICE_DATE#", with: reportDate)
-   
             // The employee data will be added by using a loop.
             var allItems = ""
 
             for i in 0..<items.count {
                 var itemHTMLContent: String!
+                var final_inImagepath: String!
+                var final_outImagepath: String!
+                
+                //get the signature image path to display
+                final_inImagepath = imagepath + items[i]["employeeName"]! + "_checkin.jpg"
+                final_outImagepath = imagepath + items[i]["employeeName"]! + "_checkout.jpg"
+               
+                let sampleImage = Bundle.main.path(forResource: "AttitudeTech", ofType: "png")
+              
+             
+                
+//                let imageHTMLContent = "<img src='/Users/ziyunhe/Library/Developer/CoreSimulator/Devices/5B2CD93F-A1E7-4D12-9B1C-23FF848253F4/data/Containers/Data/Application/C6DF8797-F3DE-49D4-969E-BF2906838DF3/Documents/Abhijith_checkout.jpg' alt='Abhijith_checkout'>"
+                
+                let imageHTMLContent = "<img src='/Users/ziyunhe/Documents/iOSStudy/AttendanceApp/AttendanceApp/XML Templates/AttitudeTech.png' alt='Abhijith_checkout'>"
+                
+//                let inImageURL = NSURL(fileURLWithPath: final_inImagepath)
+//
+//                let inImage = try Data(contentsOf: inImageURL as URL)
+////                UIImage(data: inImage)
                 
                 itemHTMLContent = try String(contentsOfFile: pathToSingleItemHTMLTemplate!)
                 
@@ -78,8 +85,10 @@ class ReportComposer: NSObject {
                 itemHTMLContent = itemHTMLContent.replacingOccurrences(of: "#CHECKOUT_TIME#", with: items[i]["checkOutTime"]!)
              
                 // Replace the employee signatire with the images.
-                itemHTMLContent = itemHTMLContent.replacingOccurrences(of: "#IN_SIGNATURE#", with: items[i]["checkOutTime"]!)
+                itemHTMLContent = itemHTMLContent.replacingOccurrences(of: "#IN_SIGNATURE#", with: imageHTMLContent)
                 
+                itemHTMLContent = itemHTMLContent.replacingOccurrences(of: "#OUT_SIGNATURE#", with: final_outImagepath)
+
                 // Add the item's HTML code to the general items string.
                 allItems += itemHTMLContent
                 print(allItems)
